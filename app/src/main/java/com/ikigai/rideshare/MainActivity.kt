0488a3b7ccc1f1f5b48951902da3ccad8db05129
mekuanenet1 <58273706+mekuanenet1@users.noreply.github.com>
2020-09-20 21:30:43 -0700

    UI second with Fragments

--- app/src/main/java/com/ikigai/rideshare/MainActivity.kt
+++ app/src/main/java/com/ikigai/rideshare/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.ikigai.rideshare
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun creatAccount(view: View) { }
+    fun signUp(view: View) {}
+
+
 }
